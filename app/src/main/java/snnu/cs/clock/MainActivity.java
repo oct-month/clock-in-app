e0be960e0f9211ddbeffb9222b4a88893be2fddf
ablocker <oct-month@qq.com>
2021-06-04 19:41:45 +0800

    First Commit

---
+++ app/src/main/java/snnu/cs/clock/MainActivity.java
@@ -0,0 +1,15 @@
+package snnu.cs.clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
